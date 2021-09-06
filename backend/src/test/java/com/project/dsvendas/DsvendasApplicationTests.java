07d72dc0c54f39d8a693575b2ca3dbabdb4f90f8
Kaike Santana <kaikemice@gmail.com>
2021-09-06 11:57:38 -0300

    Project created

---
+++ backend/src/test/java/com/project/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
